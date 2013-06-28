0a722129fc335b6a3f8f0c5b23ea6a862c8e9d57
Taylor Becker <taylor.becker@evosystemsdev.com>
2013-06-28 01:54:53 -0400

    Added option to enter fullscreen mode

--- src/com/evosysdev/testtouch/TouchTestActivity.java
+++ src/com/evosysdev/testtouch/TouchTestActivity.java
@@ -2,27 +2,43 @@ package com.evosysdev.testtouch;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 /**
  * Main activity for touch test app
+ * 
  * @author tajobe
- *
+ * 
  */
 public class TouchTestActivity extends Activity
 {
-    // the surface view tracking touches
-    private TouchSurfaceView touchView;
+    // message to send intent for fullscreen
+    private static final String FULLSCREEN_MESSAGE = "FULLSCREEN";
+    
+    private TouchSurfaceView touchView; // the surface view tracking touches
     
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        
+        // check for fullscreen flag
+        if (this.getIntent().getBooleanExtra(FULLSCREEN_MESSAGE, false))
+        {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        
+        // set content
         setContentView(R.layout.activity_touch_test);
         
-        // grab touch surface
-        touchView = (TouchSurfaceView) this.findViewById(R.id.touchView1);
+        touchView = (TouchSurfaceView) this.findViewById(R.id.touchView1); // grab touch surface
     }
     
     /**
@@ -54,9 +70,20 @@ public class TouchTestActivity extends Activity
     }
     
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle item selection
-        switch (item.getItemId()) {
+        switch (item.getItemId())
+        {
+            case R.id.action_fullscreen:
+                // hint at how to exit fullscreen
+                Toast.makeText(this, "Press back to return...", Toast.LENGTH_SHORT).show();
+                
+                // start in fullscreen mode
+                Intent fullscreenIntent = new Intent(this, TouchTestActivity.class);
+                fullscreenIntent.putExtra(FULLSCREEN_MESSAGE, true);
+                startActivity(fullscreenIntent);
+                return true;
             case R.id.action_reset:
                 touchView.reset(); // reset touches
                 return true;
@@ -64,5 +91,4 @@ public class TouchTestActivity extends Activity
                 return super.onOptionsItemSelected(item);
         }
     }
-    
 }

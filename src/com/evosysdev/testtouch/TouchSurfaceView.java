39791fc4523d007a63d2e436da8a088bacff5d08
Taylor Becker <taylor.becker@evosystemsdev.com>
2013-06-28 00:28:43 -0400

    Moved drawing off main thread
    Changed to a surfaceview to draw on a canvas provided by a surfaceholder off the main thread
    Added drawing pausing/resuming + sleeping when not being updated
    Added clear to menu(removed default "Settings" that did nothing)
    "Fixed" pressure sensitive drawing

--- src/com/evosysdev/testtouch/TouchSurfaceView.java
+++ src/com/evosysdev/testtouch/TouchSurfaceView.java
@@ -4,25 +4,46 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.graphics.Paint.Style;
+import android.os.SystemClock;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 /**
  * Simple View to draw touches
  * 
- * @author taylor
+ * @author tajobe
  * 
  */
-public class TouchView extends View
+public class TouchSurfaceView extends SurfaceView implements Runnable
 {
+    // colors to use in touches
     private static final int[] TOUCH_COLORS =
-    { Color.WHITE, Color.BLUE + 0x222200, Color.GREEN + 0x220022, Color.YELLOW + 0x22, Color.RED + 0x2222 };
-    private static final int HISTORY_LEN = 5000;
+    {
+            Color.WHITE, Color.BLUE + 0x222200, Color.GREEN + 0x220022,
+            Color.YELLOW + 0x22, Color.RED + 0x2222
+    };
+    
+    private static final int HISTORY_LEN, FRAME_TIME;
+    private static final String TAG;
+    
+    static
+    {
+        HISTORY_LEN = 10000; // number of touch points to keep in history
+        FRAME_TIME = 20; // length of time(ms) for a frame(between draws)
+        TAG = "Drawing"; // log tag/thread name
+    }
     
     private Touch[] touches; // array of recorded touches
     private int i; // current touch index
+    private volatile boolean updated; // have there been touches since last draw
+    
+    private Thread drawing; // drawing thread
+    private SurfaceHolder surface; // drawing surface
     private Paint paint; // paint to avoid constantly reallocating paint objects
     
     /**
@@ -33,13 +54,93 @@ public class TouchView extends View
      * @param attrs
      *            attributes of view
      */
-    public TouchView(Context context, AttributeSet attrs)
+    public TouchSurfaceView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+        
+        // create touch history array
         touches = new Touch[HISTORY_LEN];
-        paint = new Paint();
+        
+        // create paint
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setStyle(Style.FILL_AND_STROKE);
         paint.setTextSize(20);
+        
+        // get surface holder
+        surface = this.getHolder();
+    }
+    
+    /**
+     * Resume drawing
+     */
+    public void resume()
+    {
+        // only start drawing if we aren't already
+        if (drawing == null)
+        {
+            drawing = new Thread(this, TAG);
+            drawing.start();
+        }
+    }
+    
+    /**
+     * Pause drawing
+     */
+    public void pause()
+    {
+        // stop if we are drawing
+        if (drawing != null)
+        {
+            // interrupt drawing thread
+            drawing.interrupt();
+            
+            try
+            {
+                drawing.join(); // wait for thread to die
+                drawing = null; // nullify for later
+            }
+            catch (InterruptedException e)
+            {
+                Log.w(TAG, "Error pausing thread!", e);
+            }
+        }
+    }
+    
+    /**
+     * Run drawing thread
+     */
+    @Override
+    public void run()
+    {
+        Canvas c = null;
+        long time = 0, took = 0;
+        
+        try
+        {
+            while (!drawing.isInterrupted())
+            {
+                time = SystemClock.currentThreadTimeMillis(); // frame start time
+                
+                // ensure we have a valid surface to draw to and avoid unnecessary
+                // draws by only drawing if something has changed
+                if (surface.getSurface().isValid() && updated)
+                {
+                    c = surface.lockCanvas(); // grab canvas
+                    drawTouches(c); // draw
+                    updated = false;
+                    surface.unlockCanvasAndPost(c); // inform we are done with canvas
+                }
+                
+                took = SystemClock.currentThreadTimeMillis() - time; // length of frame
+                
+                // took less time than expected for frame, sleep remaining time
+                if (took < FRAME_TIME) Thread.sleep(FRAME_TIME - took);
+            }
+        }
+        catch (InterruptedException e)
+        {
+            // expected on pause if in sleep
+        }
     }
     
     /**
@@ -50,13 +151,16 @@ public class TouchView extends View
      * @param c
      *            canvas to draw on
      */
-    @Override
-    public void onDraw(Canvas c)
+    public void drawTouches(Canvas c)
     {
+        // clear canvas
+        c.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+        
         // draw finger labels
         int labelsTop = c.getHeight() - 30;
         int labelsBot = c.getHeight() - 10;
         float labelSpacing = 35 + (paint.getTextSize() * paint.getTextScaleX() * 3);
+        paint.setStrokeWidth(0);
         for (int i = 0; i < TOUCH_COLORS.length; i++)
         {
             float spacing = i * labelSpacing;
@@ -70,11 +174,12 @@ public class TouchView extends View
         for (Touch touch : touches)
         {
             // have not filled past here, skip rest of loop
-            if (touch == null)
-                break;
+            if (touch == null) break;
             
             paint.setColor(TOUCH_COLORS[touch.index]);
-            paint.setStrokeWidth((float) Math.log(touch.pressure * 20f));
+            float stroke = (touch.pressure * 10f) + 2;
+            stroke = stroke * stroke / 20f;
+            paint.setStrokeWidth(stroke);
             c.drawPoint(touch.x, touch.y, paint);
         }
     }
@@ -88,8 +193,7 @@ public class TouchView extends View
         int pointers = event.getPointerCount();
         
         // only handle up to 5 pointers
-        if (pointers > 5)
-            pointers = 5;
+        if (pointers > 5) pointers = 5;
         
         // we only care about move events and currently handling up to 5 fingers
         if (event.getActionMasked() == MotionEvent.ACTION_MOVE)
@@ -110,6 +214,7 @@ public class TouchView extends View
                         event.getPressure(pointer));
             }
         }
+        updated = true;
         
         return true;
     }
@@ -130,10 +235,17 @@ public class TouchView extends View
         touches[i++] = new Touch(index, x, y, pressure);
         
         // wrap around if we have reached the end of our array
-        if (i == touches.length)
-            i = 0;
-        
-        this.invalidate(); // invalidate view to force a redraw
+        if (i == touches.length) i = 0;
+    }
+    
+    /**
+     * Reset touches
+     */
+    public void reset()
+    {
+        touches = new Touch[HISTORY_LEN]; // reset touches
+        i = 0; // reset index
+        updated = true; // inform we have updated
     }
     
     /**
--- src/com/evosysdev/testtouch/TouchTestActivity.java
+++ src/com/evosysdev/testtouch/TouchTestActivity.java
@@ -3,19 +3,46 @@ package com.evosysdev.testtouch;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Main activity for touch test app
- * @author taylor
+ * @author tajobe
  *
  */
 public class TouchTestActivity extends Activity
 {
+    // the surface view tracking touches
+    private TouchSurfaceView touchView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_touch_test);
+        
+        // grab touch surface
+        touchView = (TouchSurfaceView) this.findViewById(R.id.touchView1);
+    }
+    
+    /**
+     * Resume touch tracing
+     */
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        touchView.resume();
+    }
+    
+    /**
+     * Pause touch tracing
+     */
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        touchView.pause();
     }
     
     @Override
@@ -24,6 +51,18 @@ public class TouchTestActivity extends Activity
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.touch_test, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_reset:
+                touchView.reset(); // reset touches
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
     
 }
